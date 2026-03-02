44331ce3f386a018cb861e18b8ac16e78dde5650
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-03-02 11:46:54 -0600

    work ples

--- Party.cs
+++ Party.cs
@@ -1,39 +1,22 @@
-using System;  // Needed for Console
-
-namespace knightmoves;
-
-
-public class Person
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public Person(string firstName, string lastName)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-    }
-}
-
-
+﻿namespace knightmoves;
 public class Party
 {
-    public static Person[] Guests = new Person[]
+    public Person[] Guests = new Person[]
     {
+        new Person("Jane", "Doe"),
         new Person("John", "Doe"),
-        new Person("Jane", "Smith"),
         new Person("Bob", "Johnson")
     };
 }
 
-e
-class Program
+public class Person
 {
-    static void Main()
+    public string FirstName { get; }
+    public string LastName { get; }
+
+    public Person(string FirstName, string LastName)
     {
-        Console.WriteLine("Party Guests:");
-        foreach (var guest in Party.Guests)
-        {
-            Console.WriteLine($"{guest.FirstName} {guest.LastName}");
-        }
+        FirstName = firstName;
+        LastName = lastName
     }
 }
